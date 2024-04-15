d56a98c5f97528150d5345cc59252a9889121695
ReactedTitan880 <thomasnardou@icloud.com>
2024-04-15 15:39:15 +0200

    change style of book list and add components

--- MauiApp1/MauiApp1/BookCards.xaml.cs
+++ MauiApp1/MauiApp1/BookCards.xaml.cs
@@ -2,13 +2,23 @@ namespace MauiApp1;
 
 public partial class BookCards : ContentView
 {
-	public static readonly BindableProperty BookTitleProperty = BindableProperty.Create(nameof(BookTitle), typeof(string), typeof(BookCards), string.Empty);
-	public string BookTitle
-	{
-		get => (string)GetValue(BookCards.BookTitleProperty);
-		set => SetValue(BookCards.BookTitleProperty, value);
-	} 
-	public BookCards()
+    public static readonly BindableProperty BookTitleProperty = BindableProperty.Create(nameof(BookTitle), typeof(string), typeof(BookCards), string.Empty);
+    public static readonly BindableProperty BookImagePathProperty = BindableProperty.Create(nameof(BookImagePath), typeof(string), typeof(BookCards), string.Empty);
+    //boruto.jpg
+
+    public string BookTitle
+    {
+        get => (string)GetValue(BookCards.BookTitleProperty);
+        set => SetValue(BookCards.BookTitleProperty, value);
+    }
+    public string BookImagePath
+    {
+        get => (string)GetValue(BookCards.BookImagePathProperty);
+        set => SetValue(BookCards.BookImagePathProperty, value);
+    }
+
+
+    public BookCards()
 	{
 		InitializeComponent();
 	}
--- MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -1,23 +1,46 @@
 ﻿using Newtonsoft.Json;
 using System.Text.Json.Serialization;
+using VersOne.Epub;
+using System.Text;
+using HtmlAgilityPack;
+using System.Diagnostics;
 
 namespace MauiApp1
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         private HttpClient client;
 
         public MainPage()
         {
+            
+            
             InitializeComponent();
             client = new HttpClient();
         }
 
         private async void OnCounterClicked(object sender, EventArgs e)
         {
-            await DisplayAlert("fnsepgsrh", "bsrhsrhsrh", "ok");
+            var response = await FilePicker.PickAsync(new PickOptions
+            {
+                PickerTitle = "Please chose an epub file",
+                FileTypes = null
+            });
+            Debug.WriteLine("=====================================================================");
+            Debug.WriteLine(response);
+            Debug.WriteLine("=====================================================================");
+            if (response != null && response.FileName.EndsWith("epub", StringComparison.OrdinalIgnoreCase))
+            {
+                EpubBook book = EpubReader.ReadBook(response.FullPath);
+                
+                Debug.WriteLine("=====================================================================");
+                Debug.WriteLine(book.Title);
+                Debug.WriteLine(book.CoverImage);
+                Debug.WriteLine("=====================================================================");
+                await DisplayAlert("Merci pour votre séléction", "Vous avez choisi " + book.Title + " de " + book.Author, "OK");
+            }
+
         }
     }
 
